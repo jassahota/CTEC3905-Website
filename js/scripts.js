cc55bfb306c2b95e17cd97594477866ee0eb5976
jas sahota <jas.sahota95@gmail.com>
2017-12-06 12:23:27 +0000

    add of new api for football

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,37 @@
   }
 
 })();
+
+
+"use strict";
+
+(function(){
+  
+  let queryBox = document.getElementById("guardQuery");
+  let footballJSON = document.getElementById("football-news");
+
+  let baseURL = "http://content.guardianapis.com/search?page-size=3&q=football&api-key=cbb1c233-0012-4e70-8c33-bb63d607fe35";
+
+	addEventListener("submit", function(ev){
+    let url = baseURL + queryBox.value;
+    let request = new Request(url);
+    fetch(request)
+      .then(function (response) {
+        // console.log(`response: ${response.status}`);
+        return response.json();
+      })
+      .then(function(data) {
+        // console.log(data);
+        let key;
+        let theData = "";
+        let tmp = data.response.results;
+        for (key in tmp) {
+          theData += `<li><a href="${tmp[key].webUrl}">${tmp[key].webTitle}</a> ${tmp[key].webPublicationDate}</li>`;
+        }
+        footballJSON.innerHTML = theData;
+      });
+	  queryBox.value = "";
+    ev.preventDefault();
+  }, false);
+
+}());
