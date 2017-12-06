d6b2c1b5183938fa1e7997c5241b9fc0ae73620c
jas sahota <jas.sahota95@gmail.com>
2017-12-06 12:27:19 +0000

    update apis

--- js/scripts.js
+++ js/scripts.js
@@ -24,7 +24,7 @@
 
 (function(){
   
-  let queryBox = document.getElementById("guardQuery");
+  let queryBox = document.getElementById("footballQuery");
   let footballJSON = document.getElementById("football-news");
 
   let baseURL = "http://content.guardianapis.com/search?page-size=3&q=football&api-key=cbb1c233-0012-4e70-8c33-bb63d607fe35";
@@ -46,6 +46,72 @@
           theData += `<li><a href="${tmp[key].webUrl}">${tmp[key].webTitle}</a> ${tmp[key].webPublicationDate}</li>`;
         }
         footballJSON.innerHTML = theData;
+      });
+	  queryBox.value = "";
+    ev.preventDefault();
+  }, false);
+
+}());
+
+
+//f1
+(function(){
+  
+  let queryBox = document.getElementById("f1Query");
+  let f1JSON = document.getElementById("f1-news");
+
+  let baseURL = "http://content.guardianapis.com/search?page-size=3&q=f1&api-key=cbb1c233-0012-4e70-8c33-bb63d607fe35";
+
+	addEventListener("submit", function(ev){
+    let url = baseURL + queryBox.value;
+    let request = new Request(url);
+    fetch(request)
+      .then(function (response) {
+        // console.log(`response: ${response.status}`);
+        return response.json();
+      })
+      .then(function(data) {
+        // console.log(data);
+        let key;
+        let theData = "";
+        let tmp = data.response.results;
+        for (key in tmp) {
+          theData += `<li><a href="${tmp[key].webUrl}">${tmp[key].webTitle}</a> ${tmp[key].webPublicationDate}</li>`;
+        }
+        f1JSON.innerHTML = theData;
+      });
+	  queryBox.value = "";
+    ev.preventDefault();
+  }, false);
+
+}());
+
+
+//basketball
+(function(){
+  
+  let queryBox = document.getElementById("basketballQuery");
+  let basketballJSON = document.getElementById("basketball-news");
+
+  let baseURL = "http://content.guardianapis.com/search?page-size=3&q=basketball&api-key=cbb1c233-0012-4e70-8c33-bb63d607fe35";
+
+	addEventListener("submit", function(ev){
+    let url = baseURL + queryBox.value;
+    let request = new Request(url);
+    fetch(request)
+      .then(function (response) {
+        // console.log(`response: ${response.status}`);
+        return response.json();
+      })
+      .then(function(data) {
+        // console.log(data);
+        let key;
+        let theData = "";
+        let tmp = data.response.results;
+        for (key in tmp) {
+          theData += `<li><a href="${tmp[key].webUrl}">${tmp[key].webTitle}</a> ${tmp[key].webPublicationDate}</li>`;
+        }
+        basketballJSON.innerHTML = theData;
       });
 	  queryBox.value = "";
     ev.preventDefault();
